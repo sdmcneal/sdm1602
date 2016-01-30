c485efe66bbaeedbf9dc92a423a0a648a0c76a3d
sdmcneal <scott.mcneal@gmail.com>
2016-01-30 09:22:20 -0800

    segregated detail component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,29 +1,22 @@
 import {Component} from 'angular2/core';
-interface Hero {
-  id: number;
-  name: string;
-}
+import {Hero} from './hero';
+import {HeroDetailComponent} from "./hero-detail.component";
+
+
 @Component({
   selector: 'my-app',
   template:`
-    <h1>{{title}}</h1>
-    <h2>My Heroes</h2>
-    <ul class="heroes">
-      <li *ngFor="#hero of heroes"
-        [class.selected]="hero === selectedHero"
-        (click)="onSelect(hero)">
-        <span class="badge">{{hero.id}}</span> {{hero.name}}
-      </li>
-    </ul>
-    <div *ngIf="selectedHero">
-      <h2>{{selectedHero.name}} details!</h2>
-      <div><label>id: </label>{{selectedHero.id}}</div>
-      <div>
-        <label>name: </label>
-        <input [(ngModel)]="selectedHero.name" placeholder="name"/>
-      </div>
-    </div>
-  `,
+  <h1>{{title}}</h1>
+  <h2>My Heroes</h2>
+  <ul class="heroes">
+    <li *ngFor="#hero of heroes"
+      [class.selected]="hero === selectedHero"
+      (click)="onSelect(hero)">
+      <span class="badge">{{hero.id}}</span> {{hero.name}}
+    </li>
+  </ul>
+  <my-hero-detail [hero]="selectedHero"></my-hero-detail>
+`,
   styles:[`
     .selected {
       background-color: #CFD8DC !important;
@@ -71,7 +64,8 @@ interface Hero {
       margin-right: .8em;
       border-radius: 4px 0px 0px 4px;
     }
-  `]
+  `],
+    directives: [HeroDetailComponent]
 })
 export class AppComponent {
   public title = 'Tour of Heroes';
